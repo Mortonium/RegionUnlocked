94e80a639e752f1c8815e92e070a66d76b55236e
Alex Morton-Cooke <mortoniumcook@gmail.com>
2013-05-26 00:16:54 +0100

    iugb

--- src/com/hack/regionunlocked/GameStatus.java
+++ src/com/hack/regionunlocked/GameStatus.java
@@ -194,7 +194,7 @@ public class GameStatus extends AsyncTask<Void, Void, Boolean> {
 					"<td bgcolor=\"#[A-F0-9]*\">[\\s]*([\\w?]+)[\\s]*</td>"; // PAL
 																				// compatibility
 
-			regex = "<td>[\\s]*<a href=\"[^\"]*\"[^>]*>Halo 4</a>[\\s]*</td>[\\s]*<td>[\\s]*([\\w]+)[\\s]*</td>[\\s]*<td bgcolor=\"#[A-F0-9]*\">[\\s]*([\\w?]+)[\\s]*</td>[\\s]*<td bgcolor=\"#[A-F0-9]*\">[\\s]*([\\w?]+)[\\s]*</td>[\\s]*<td bgcolor=\"#[A-F0-9]*\">[\\s]*([\\w?]+)[\\s]*</td>";
+			regex = "<td>[\\s]*<a href=\"[^\"]*\"[^>]*>Halo 4</a>[\\s]*</td>[\\s]*<td>[\\s]*([\\w]+)[\\s]*</td>[\\s]*<td bgcolor=\"#[A-F0-9]*\">[\\s]*([\\w]+)[\\s]*</td>[\\s]*<td bgcolor=\"#[A-F0-9]*\">[\\s]*([\\w]+)[\\s]*</td>[\\s]*<td bgcolor=\"#[A-F0-9]*\">[\\s]*([\\w]+)[\\s]*</td>";
 			Pattern pattern = Pattern.compile(regex);
 			Matcher matcher = pattern.matcher(content);
 			
